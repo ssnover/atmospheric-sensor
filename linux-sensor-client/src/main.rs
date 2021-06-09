583f576c2a91862323a6408d21767d1ffc02d0e0
Shane Snover <ssnover95@gmail.com>
2021-06-08 20:03:42 -0600

    Hacked together an async serial port by creating a tokio file from a raw fd

--- linux-sensor-client/src/main.rs
+++ linux-sensor-client/src/main.rs
@@ -1,3 +1,26 @@
-fn main() {
-    println!("Hello, world!");
+use std::os::unix::io::{FromRawFd, IntoRawFd};
+use tokio::io::{AsyncReadExt};
+
+#[tokio::main]
+async fn main() {
+    let builder = serialport::new("/tmp/atmospheric-sensor-sim", 115200);
+    let port = builder.open_native().unwrap();
+    let mut port = unsafe {tokio::fs::File::from_raw_fd(port.into_raw_fd()) };
+
+    tokio::select! {
+        _ = listen(&mut port) => {
+            println!("Simulator exited");
+        }
+        _ = tokio::signal::ctrl_c() => {
+            println!("Received SIGINT");
+        }
+    }
+}
+
+async fn listen(port: &mut tokio::fs::File) -> std::io::Result<()> {
+    loop {
+        let mut buf: [u8; 1024] = [0; 1024];
+        let num_bytes = port.read(&mut buf[..]).await?;
+        println!("Received {} bytes", num_bytes);
+    }
 }
